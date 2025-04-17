9c10512973376aeceb825b9063082391298d6c24
StreallyX <aproject37@gmail.com>
2025-04-17 17:39:56 +0200

    add package

--- dz-yr/next.config.ts
+++ dz-yr/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['iklmneqgjiugiksfnuqy.supabase.co'], // ton domaine Supabase ici
+    domains: ['iklmneqgjiugiksfnuqy.supabase.co'],
   },
 }
 
